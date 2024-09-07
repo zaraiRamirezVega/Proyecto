67b36d4453960cc75ea426dda725b6348a24de22
zaraiRamirezVega <1518103@senati.pe>
2024-09-06 18:10:58 -0900

    fourth commit

--- myapp/forms.py
+++ myapp/forms.py
@@ -3,19 +3,23 @@ from django import forms
 class RegisterForm(forms.Form):
     username = forms.CharField(
         label='Usuario', 
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-input'})
     )
     password = forms.CharField(
         label='Contraseña',
-        widget=forms.PasswordInput
+        widget=forms.PasswordInput(attrs={'class': 'form-input'})
     )
     age = forms.IntegerField(
-        label='Edad'
+        label='Edad',
+        widget=forms.NumberInput(attrs={'class': 'form-input'})
     )
     gender = forms.ChoiceField(
         label='Género',
-        choices=[('hombre', 'Masculino'), ('mujer', 'Femenino')]
+        choices=[('hombre', 'Masculino'), ('mujer', 'Femenino')],
+        widget=forms.Select(attrs={'class': 'form-select'})
     )
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(
